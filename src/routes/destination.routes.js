bff5c892ca90689dc952a18d2da550d8257d7254
Julia Ribeiro <53882202+JuliaYuki@users.noreply.github.com>
2024-05-11 19:35:57 -0300

    Implementação das rotas GET, PUT

--- src/routes/destination.routes.js
+++ src/routes/destination.routes.js
@@ -4,9 +4,10 @@ const { auth } = require("../middleware/auth");
 
 const destinationRoute = new Router();
 
-destinationRoute.post("/", async (req, res) => {
+destinationRoute.post("/", auth, async (req, res) => {
   try {
-    const { destination, description, user_id } = req.body;
+    const { destination, description } = req.body;
+    const user_id = req.payload.sub;
 
     if (!destination || !description || !user_id) {
       return res
@@ -26,6 +27,80 @@ destinationRoute.post("/", async (req, res) => {
     return res
       .status(500)
       .json({ error: "Não foi possível cadastrar o destino" });
+  }
+});
+
+destinationRoute.get("/", auth, async (req, res) => {
+  try {
+    const user_id = req.payload.sub;
+    const destinos = await Destino.findAll({
+      where: {
+        user_id: user_id,
+      },
+    });
+
+    return res.status(200).json(destinos);
+  } catch (error) {
+    console.log("Erro ao listar destinos:", error);
+    return res
+      .status(500)
+      .json({ error: "Não foi possível listar os destinos" });
+  }
+});
+
+destinationRoute.get("/:destino_id", auth, async (req, res) => {
+  try {
+    const user_id = req.payload.sub;
+    const destino_id = req.params.destino_id;
+
+    const destino = await Destino.findOne({
+      where: {
+        id: destino_id,
+        user_id: user_id,
+      },
+    });
+
+    if (!destino) {
+      return res.status(404).json({ error: "Destino não encontrado!" });
+    }
+
+    return res.status(200).json(destino);
+  } catch (error) {
+    console.log("Erro ao obter informações do destino:", error);
+    return res
+      .status(500)
+      .json({ error: "Não foi possível obter informações do destino" });
+  }
+});
+
+destinationRoute.put("/:destino_id", auth, async (req, res) => {
+  try {
+    const user_id = req.payload.sub;
+    const destino_id = req.params.destino_id;
+    const { destination, description } = req.body;
+
+    const destino = await Destino.findOne({
+      where: {
+        id: destino_id,
+        user_id: user_id,
+      },
+    });
+
+    if (!destino) {
+      return res.status(404).json({ error: "Destino não encontrado!" });
+    }
+
+    destino.destination = destination;
+    destino.destination = description;
+
+    await destino.save();
+
+    return res.status(200).json(destino);
+  } catch (error) {
+    console.log("Erro ao obter informações do destino:", error);
+    return res
+      .status(500)
+      .json({ error: "Não foi possível obter informações do destino" });
   }
 });
 
